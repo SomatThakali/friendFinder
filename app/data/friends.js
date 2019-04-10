c50b183f1655016d563ed3fa65350a71c01231af
Somat Thakali <sthakal000@citymail.cuny.edu>
2019-04-10 18:12:15 -0400

    Created friends array

---
+++ app/data/friends.js
@@ -0,0 +1,60 @@
+var freindsArray = [
+  {
+    name: "Ahmed",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+  },
+  {
+    name: "Aman",
+    photo:
+      "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    scores: ["5", "5", "4", "1", "2", "3", "3", "4", "5", "2"]
+  },
+  {
+    name: "Somat",
+    photo:
+      "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
+  },
+  {
+    name: "Bob",
+    photo: "http://murderpedia.org/male.H/images/hall_erick/hall000.jpg",
+    scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+  },
+  {
+    name: "Johncena",
+    photo: "https://images.app.goo.gl/b3QLPpm6MPrhDryPA",
+    scores: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
+  },
+  {
+    name: "Bikson",
+    photo:
+      "https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    scores: ["5", "3", "3", "1", "4", "1", "5", "1", "2", "4"]
+  },
+  {
+    name: "Hernan",
+    photo:
+      "https://news.nationalgeographic.com/content/dam/news/2018/05/17/you-can-train-your-cat/02-cat-training-NationalGeographic_1484324.jpg",
+    scores: ["5", "3", "3", "1", "4", "1", "5", "1", "2", "4"]
+  },
+  {
+    name: "Deming",
+    photo:
+      "https://cms.qz.com/wp-content/uploads/2019/02/RTS1P96N-e1551211588926.jpg?quality=75&strip=all&w=1600&h=900&crop=1",
+    scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+  },
+  {
+    name: "Ritter",
+    photo: "https://modernfarmer.com/wp-content/uploads/2014/03/smart_pig.jpg",
+    scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+  },
+  {
+    name: "Erick",
+    photo: "http://i.imgur.com/2OtDU.jpg",
+    scores: ["2", "3", "3", "3", "2", "4", "3", "3", "2", "1"]
+  }
+];
+
+module.exports = freindsArray;
