db2fe419d72d53a4bb7bdfc64ed292e1bec8102c
Somat Thakali <sthakal000@citymail.cuny.edu>
2019-04-11 15:15:51 -0400

    Removed a questions div

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 7100;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
