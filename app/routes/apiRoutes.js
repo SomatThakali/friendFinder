60f8d2c15bd9055a4ebe7e757e4284c693492c73
Somat Thakali <sthakal000@citymail.cuny.edu>
2019-04-15 12:21:26 -0400

    Fixed best mathc logic

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -6,8 +6,33 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
-    friendsData.push(req.body);
-    console.log(req.body);
+    // friendsData.push(req.body);
+
+    let userInput = req.body;
+
+    console.log(friendsData[0].scores);
+    console.log(userInput.scores);
+
+    let matchName = "";
+    let matchImage = "";
+    let maxDiff = 40;
+
+    for (let i = 0; i < friendsData.length; i++) {
+      let minDiff = 0;
+      for (let j = 0; j < userInput.scores.length; j++) {
+        minDiff += Math.abs(friendsData[i].scores[j] - userInput.scores[j]);
+      }
+
+      if (minDiff < maxDiff) {
+        maxDiff = minDiff;
+        matchName = friendsData[i].name;
+        matchImage = friendsData[i].photo;
+      }
+    }
+
+    friendsData.push(userInput);
+
+    res.json({ status: "OK", name: matchName, photo: matchImage });
   });
 
   app.post("/api/clear", function(req, res) {
